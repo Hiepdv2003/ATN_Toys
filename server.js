efe5ccdaf50c40f1ce4f4b95579e08661cc48aa3
Hiepdv2003 <hiepdvgcs210944@fpt.edu.vn>
2023-06-30 11:49:33 +0700

    New

--- server.js
+++ server.js
@@ -1,86 +1,90 @@
 const express = require('express');
-const bodyParser = require('body-parser')
 const app = express();
+const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require('mongodb').ObjectId;
 
-const MongoClient = require('mongodb').MongoClient
-
-const connectionString = 'mongodb+srv://hiepdvgcs210944:gXzSOHyt3townFC0@cluster0.y682z3s.mongodb.net/'
+const connectionString = 'mongodb+srv://hiepdvgcs210944:gXzSOHyt3townFC0@cluster0.y682z3s.mongodb.net/';
 
 MongoClient.connect(connectionString, { useUnifiedTopology: true })
     .then(client => {
-        console.log('Connected to Database')
+        console.log('Connected to Database');
 
-        const db = client.db('ATN')
-        const toysCollection = db.collection('toys')
+        const db = client.db('ATN');
+        const toysCollection = db.collection('toys');
 
-        app.set('view engine', 'ejs')
-
-        app.use(bodyParser.urlencoded({ extended: true }))
-
-        app.use(express.static('public'))
-
-        app.use(bodyParser.json())
+        app.set('view engine', 'ejs');
+        app.use(express.urlencoded({ extended: true }));
+        app.use(express.static('public'));
+        app.use(express.json());
 
         app.get('/', (req, res) => {
-            toysCollection.find().toArray()
+            toysCollection
+                .find()
+                .toArray()
                 .then(results => {
-                    console.log(results)
-                    res.render('pages/index', { toys: results })
+                    console.log(results);
+                    res.render('pages/index', { toys: results });
                 })
-                .catch(error => console.error(error))
-        })
+                .catch(error => console.error(error));
+        });
 
-        app.get('/add', function (req, res) {
+        app.get('/add', (req, res) => {
             res.render('pages/add');
-        })
+        });
 
         app.post('/save', (req, res) => {
-            toysCollection.insertOne(req.body)
+            toysCollection
+                .insertOne(req.body)
                 .then(result => {
-                    console.log(result)
-                    res.redirect('/add')
+                    console.log(result);
+                    res.redirect('/');
                 })
-                .catch(error => console.error(error))
-        })
+                .catch(error => console.error(error));
+        });
 
-        app.get('/edit/:id', function (req, res) {
-            const toyId = parseInt(req.params.id);
-            toysCollection.findOne({ toyId: toyId })
+        app.get('/edit/:id', (req, res) => {
+            const toyId = req.params.id;
+            toysCollection
+                .findOne({ _id: new ObjectId(toyId) })
                 .then(toy => {
                     res.render('pages/edit', { toy: toy });
                 })
                 .catch(error => console.error(error));
-        })
+        });
 
         app.post('/edit/:id', (req, res) => {
-            const toyId = parseInt(req.params.id);
-            toysCollection.findOneAndUpdate(
-                { toyId: toyId },
-                {
-                    $set: {
-                        name: req.body.name,
-                        price: req.body.price
-                    }
-                },
-                { useFindAndModify: false }
-            )
-                .then(result => res.json('Updated Successfully'))
-                .catch(error => console.error(error))
-        })
-
-        app.delete('/delete/:id', (req, res) => {
-            const toyId = parseInt(req.params.id);
-            toysCollection.findOneAndDelete(
-                { toyId: toyId },
-                { useFindAndModify: false }
-            )
+            const toyId = req.params.id;
+            toysCollection
+                .findOneAndUpdate(
+                    { _id: new ObjectId(toyId) },
+                    {
+                        $set: {
+                            name: req.body.name,
+                            price: req.body.price,
+                            image: req.body.image
+                        }
+                    },
+                    { useFindAndModify: false }
+                )
                 .then(result => {
-                    res.json('Deleted Successfully')
+                    console.log(result);
+                    res.redirect('/');
                 })
-                .catch(error => console.error(error))
-        })
+                .catch(error => console.error(error));
+        });
 
-        app.listen(8080, function () {
-            console.log('listening on 8080')
-        })
-    })
+        app.get('/delete/:id', (req, res) => {
+            const toyId = req.params.id;
+            toysCollection
+                .findOneAndDelete({ _id: new ObjectId(toyId) })
+                .then(result => {
+                    console.log(result);
+                    res.redirect('/');
+                })
+                .catch(error => console.error(error));
+        });
+
+        app.listen(8080, () => {
+            console.log('listening on 8080');
+        });
+    });
